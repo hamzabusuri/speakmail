6eedf29b6a9f22cc2b3bcd974eb0c753674f7614
hb308 <hamzabusuri96@hotmail.com>
2019-12-25 18:07:56 +0000

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$k8sejxr@tcz_m(f=^x$iovc3fzo#!xri29vc!7(lehy4h!2)6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['speakmail.herokuapp.com','127.0.0.1']
 
