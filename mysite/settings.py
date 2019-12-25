da6ffc511c4cc419a4a003dc03190e4a3a736d59
hb308 <hamzabusuri96@hotmail.com>
2019-12-25 17:04:11 +0000

    Modifying for connection issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,7 @@ USE_TZ = True
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
